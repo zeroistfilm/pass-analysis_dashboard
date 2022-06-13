98b39b1fd96831f6b055b7f90937ee28e32d02df
YD <zeroistfilm@naver.com>
2022-06-13 21:54:35 +0900

    fix address

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
     import Menulist from "./component/Menulist.svelte";
     import {itemDict} from "./store/store.js"
     import {LineChart} from "@carbon/charts-svelte";
+    import Totalinfo from "./component/Totalinfo.svelte";
 
     let isHamburger;
 
@@ -35,7 +36,7 @@
 
         {#if $itemDict['isOtherFunction'] }
             <div class="otherFunction">
-
+                <Totalinfo/>
             </div>
         {/if}
 
@@ -50,164 +51,7 @@
     </div>
 
 </main>
-<LineChart
-        data={[
-	{
-		"group": "Dataset 1",
-		"date": "2018-12-31T15:00:00.000Z",
-		"value": 50000,
-		"surplus": 1151643319.3614464
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-04T15:00:00.000Z",
-		"value": 65000,
-		"surplus": 133921672.67399856
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-07T15:00:00.000Z",
-		"value": null,
-		"surplus": 8956.237573331999
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-12T15:00:00.000Z",
-		"value": 49213,
-		"surplus": 768478046.5158633
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-16T15:00:00.000Z",
-		"value": 51213,
-		"surplus": 1188835584.0899181
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-01T15:00:00.000Z",
-		"value": 0,
-		"surplus": 3660.158337539393
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-05T15:00:00.000Z",
-		"value": 57312,
-		"surplus": 946425683.8324697
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-07T15:00:00.000Z",
-		"value": 27432,
-		"surplus": 165744218.62929156
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-14T15:00:00.000Z",
-		"value": 70323,
-		"surplus": 1154144818.5747795
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-18T15:00:00.000Z",
-		"value": 21300,
-		"surplus": 164476861.6609292
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2018-12-31T15:00:00.000Z",
-		"value": 40000,
-		"surplus": 743893903.4220402
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-04T15:00:00.000Z",
-		"value": null,
-		"surplus": 24521.985771198284
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-07T15:00:00.000Z",
-		"value": 18000,
-		"surplus": 17264805.405406658
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-12T15:00:00.000Z",
-		"value": 39213,
-		"surplus": 934203.6401497436
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-16T15:00:00.000Z",
-		"value": 61213,
-		"surplus": 1526296414.7703688
-	},
-	{
-		"group": "Dataset 4",
-		"date": "2019-01-01T15:00:00.000Z",
-		"value": 20000,
-		"surplus": 332276006.53242874
-	},
-	{
-		"group": "Dataset 4",
-		"date": "2019-01-05T15:00:00.000Z",
-		"value": 37312,
-		"surplus": 736436104.4097065
-	},
-	{
-		"group": "Dataset 4",
-		"date": "2019-01-07T15:00:00.000Z",
-		"value": 51432,
-		"surplus": 427875838.14166486
-	},
-	{
-		"group": "Dataset 4",
-		"date": "2019-01-14T15:00:00.000Z",
-		"value": 25332,
-		"surplus": 230702195.88104504
-	},
-	{
-		"group": "Dataset 4",
-		"date": "2019-01-18T15:00:00.000Z",
-		"value": null,
-		"surplus": 21278.01423861022
-	}
-]}
-        options={{
-	"title": "Thresholds (line)",
-	"axes": {
-		"bottom": {
-			"title": "2019 Annual Sales Figures",
-			"mapsTo": "date",
-			"scaleType": "time",
-			"thresholds": [
-				{
-					"value": "2019-01-10T15:00:00.000Z",
-					"label": "Custom formatter"
-				}
-			]
-		},
-		"left": {
-			"mapsTo": "value",
-			"title": "Conversion rate",
-			"scaleType": "linear",
-			"thresholds": [
-				{
-					"value": 55000,
-					"label": "Custom label",
-					"fillColor": "orange"
-				},
-				{
-					"value": 10000,
-					"fillColor": "#03a9f4"
-				}
-			]
-		}
-	},
-	"curve": "curveMonotoneX",
-	"height": "400px"
-}}
-/>
+
 <style>
     .container {
         display: flex;
---
+++ src/component/Totalinfo.svelte
@@ -0,0 +1,137 @@
+<script>
+
+</script>
+
+<div class="cards">
+    <div class="cardItem" id="sale">
+        <p class="itemTitle">현재 활동량</p>
+        <p class="itemValue">684</p>
+        <p class="itemValue">전일 대비 증감 ▲20%</p>
+    </div>
+    <div class="cardItem" id="rent">
+        <p class="itemTitle">현재 추정 평균 무게</p>
+        <p class="itemValue">32154px</p>
+        <p class="itemValue">전일 대비 증감 ▲3%</p>
+    </div>
+    <div class="cardItem" id="customer">
+        <p class="itemTitle">현재 온도</p>
+        <p class="itemValue">5732</p>
+    </div>
+    <div class="cardItem" id="agent">
+        <p class="itemTitle">현재 사료, 물 소비량</p>
+        <p class="itemValue">50kg/H, 20L/H</p>
+    </div>
+</div>
+
+
+<style>
+
+    .cards {
+        /* Cards */
+
+
+        /* Auto layout */
+
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        flex-wrap: wrap;
+        padding: 0px;
+        gap: 25px;
+
+        /*!*position: absolute;*!*/
+        /*width: 1140px;*/
+        /*height: 108.89px;*/
+        /*left: 275px;*/
+        /*top: 143px;*/
+
+
+
+    }
+    .cardItem{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 13px;
+        gap: 10px;
+
+        width: 250px;
+
+
+        /* White color */
+
+        background: #FCFCFC;
+        border-radius: 15px;
+
+    }
+
+    #sale {
+
+        flex: none;
+        order: 0;
+        flex-grow: 1;
+    }
+
+    #rent {
+        flex: none;
+        order: 1;
+        flex-grow: 1;
+    }
+
+    #customer {
+        flex: none;
+        order: 2;
+        flex-grow: 1;
+    }
+
+    #agent {
+        flex: none;
+        order: 3;
+        flex-grow: 1;
+    }
+
+
+    .itemTitle { /* Frame 18 *
+
+        /* Auto layout */
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 0px;
+        gap: 5px;
+
+        width: 149.25px;
+
+
+        /* Inside auto layout */
+
+        flex: none;
+        order: 0;
+        flex-grow: 1;
+    }
+
+    .itemValue {
+        width: 149.25px;
+        height: 33px;
+
+
+        font-style: normal;
+        font-weight: 700;
+        line-height: 33px;
+        /* identical to box height */
+
+
+        /* Text Primary color */
+
+        color: #11142D;
+
+
+        /* Inside auto layout */
+
+        flex: none;
+        order: 1;
+        align-self: stretch;
+        flex-grow: 0;
+    }
+</style>
