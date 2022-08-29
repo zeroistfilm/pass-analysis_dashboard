8b77dd5e83bd2cb9ed308aeed412700c6d33e695
YD <zeroistfilm@naver.com>
2022-08-29 10:37:51 +0900

    v2.1.4 change sample rate

--- src/component/DatesInfo.svelte
+++ src/component/DatesInfo.svelte
@@ -176,7 +176,7 @@
         //3.36.242.203:8000
         const res = await fetch(`https://api.uniai.co.kr/api/activitywithdate`, {
             method: 'POST',
-            body: JSON.stringify({dates: selectedDateArray, farm: farm, sampleRate: 0.4}),
+            body: JSON.stringify({dates: selectedDateArray, farm: farm, sampleRate: 0.2}),
             headers: {'Content-Type': 'application/json'}
         })
 
